f478e16b26c3fe4ef78fa3c16d61ac86b907a9db
Ekaterina Tolchanova <sunegina.katerin@mail.ru>
2023-06-05 22:36:02 +0300

    Task3 Firefox

--- QA_lesson_1/UnitTest1.cs
+++ QA_lesson_1/UnitTest1.cs
@@ -5,26 +5,29 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.IE;
 using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Firefox;
 
 namespace QA_lesson_1
 {
     [TestFixture]
     public class MyFirstTest
     {
-        private IWebDriver driver;
+        private WebDriver driver;
         private WebDriverWait wait;
 
         [SetUp]
         public void start()
         {
-            driver = new InternetExplorerDriver();
+            FirefoxOptions options = new FirefoxOptions();
+            options.BrowserExecutableLocation = ("C:\\Program Files\\Mozilla Firefox\\firefox.exe"); //location where Firefox is installed
+            driver = new FirefoxDriver(options);
             wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
         }
 
         [Test]
         public void FirstTest()
         {
-            driver.Url = "http://www.google.com/";
+            driver.Navigate().GoToUrl("http://www.google.com/");
             driver.FindElement(By.Name("q")).SendKeys("webdriver" + Keys.Enter);
         }
 
