8e0bfb6d3cb0311b479e5ecc701d9c1f1d85bf27
Ekaterina Tolchanova <sunegina.katerin@mail.ru>
2023-06-09 21:12:05 +0300

    Задание 4 Все пункты админки

--- QA_lesson_1/UnitTest1.cs
+++ QA_lesson_1/UnitTest1.cs
@@ -5,6 +5,8 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.IE;
 using OpenQA.Selenium.Support.UI;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace QA_lesson_1
 {
@@ -17,17 +19,38 @@ namespace QA_lesson_1
         [SetUp]
         public void start()
         {
-            driver = new InternetExplorerDriver();
+            driver = new ChromeDriver();
             wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
         }
 
         [Test]
         public void FirstTest()
         {
-            driver.Url = "http://www.google.com/";
-            driver.FindElement(By.Name("q")).SendKeys("webdriver" + Keys.Enter);
-        }
+            driver.Url = "http://localhost/litecart/admin/";
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            driver.FindElement(By.Name("login")).Click();
 
+            var elements = driver.FindElements(By.Id("app-"));
+            int a = elements.Count();
+
+
+            for (int i = 0; i < a; i++)
+            {
+                elements[i].Click();
+                elements = driver.FindElements(By.Id("app-"));
+                driver.FindElement(By.TagName("h1"));
+
+                var DocsElement = elements[i].FindElements(By.TagName("li"));
+                for (int j = 0; j < DocsElement.Count(); j++)
+                {
+                    DocsElement[j].Click();
+                    elements = driver.FindElements(By.Id("app-"));
+                    DocsElement = elements[i].FindElements(By.TagName("li"));
+                    driver.FindElement(By.TagName("h1"));
+                }
+            }
+        }
         [TearDown]
         public void stop()
         {
