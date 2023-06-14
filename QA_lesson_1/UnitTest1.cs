c1ef6745d667b672de80484df59d4d1a3411fe69
Ekaterina Tolchanova <sunegina.katerin@mail.ru>
2023-06-14 21:02:54 +0300

    Задание 7. Проверка стикеров у уточек

--- QA_lesson_1/UnitTest1.cs
+++ QA_lesson_1/UnitTest1.cs
@@ -5,6 +5,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.IE;
 using OpenQA.Selenium.Support.UI;
+using System.Linq;
 
 namespace QA_lesson_1
 {
@@ -22,10 +23,45 @@ namespace QA_lesson_1
         }
 
         [Test]
-        public void FirstTest()
+        public void LitecartCheckStikers()
         {
-            driver.Url = "http://www.google.com/";
-            driver.FindElement(By.Name("q")).SendKeys("webdriver" + Keys.Enter);
+            driver.Url = "http://localhost/litecart/";
+
+            By Popular = By.Id("box-most-popular");
+            By Campaigns = By.Id("box-campaigns");
+            By Latest = By.Id("box-latest-products");
+
+
+            var elements = driver.FindElement(Popular);
+            var PopEl = elements.FindElement(By.TagName("ul")).FindElements(By.TagName("li"));
+
+            for (int i = 0; i < PopEl.Count(); i++)
+            {
+                var PopElStickerCheck = PopEl[i].FindElements(By.ClassName("sticker"));
+                if (PopElStickerCheck.Count() != 1) Console.WriteLine("Popular: sticker error");
+
+            }
+
+            elements = driver.FindElement(Campaigns);
+            var CampEl = elements.FindElement(By.TagName("ul")).FindElements(By.TagName("li"));
+
+            for (int i = 0; i < CampEl.Count(); i++)
+            {
+                var CampElStickerCheck = CampEl[i].FindElements(By.ClassName("sticker"));
+                if (CampElStickerCheck.Count() != 1) Console.WriteLine("Campaigns: sticker error");
+
+            }
+
+            elements = driver.FindElement(Latest);
+            var LatestEl = elements.FindElement(By.TagName("ul")).FindElements(By.TagName("li"));
+
+            for (int i = 0; i < LatestEl.Count(); i++)
+            {
+                var LatestElStickerCheck = LatestEl[i].FindElements(By.ClassName("sticker"));
+                if (LatestElStickerCheck.Count() != 1) Console.WriteLine("Latest: sticker error");
+
+            }
+
         }
 
         [TearDown]
