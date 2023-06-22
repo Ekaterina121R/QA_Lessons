7c6d0bdd044a152b51bec1b4088c1e74d9b8f398
Ekaterina Tolchanova <sunegina.katerin@mail.ru>
2023-06-22 17:39:32 +0300

    Задание 8. Проверка сортировки стран и геозон

--- QA_lesson_1/UnitTest1.cs
+++ QA_lesson_1/UnitTest1.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using NUnit.Framework;
-using OpenQA.Selenium;
+﻿using NUnit.Framework;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.IE;
 using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 
 namespace QA_lesson_1
 {
@@ -17,15 +18,71 @@ namespace QA_lesson_1
         [SetUp]
         public void start()
         {
-            driver = new InternetExplorerDriver();
-            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            driver = new ChromeDriver();
         }
 
         [Test]
-        public void FirstTest()
+
+        public void Countries()
         {
-            driver.Url = "http://www.google.com/";
-            driver.FindElement(By.Name("q")).SendKeys("webdriver" + Keys.Enter);
+            // Заходим на сайт
+            driver.Url = "http://localhost/litecart/admin/";
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            driver.FindElement(By.Name("login")).Click();
+
+            driver.Url = "http://localhost/litecart/admin/?app=countries&doc=countries";
+
+
+            var Rows = driver.FindElements(By.XPath("//table[@class='dataTable']/tbody/tr[@class='row']"));
+            List<string> Countries = new List<string>();
+            List<string> Links = new List<string>();
+
+            for (int k = 0; k < Rows.Count(); k++)
+            {
+                var row = Rows[k];
+                var country = row.FindElement(By.XPath("./td[5]/a")).GetAttribute("textContent");
+                Countries.Add(country);
+
+                var zone = row.FindElement(By.XPath("./td[6]")).GetAttribute("textContent");
+                if (zone != "0") Links.Add(row.FindElement(By.XPath("./td[5]/a")).GetAttribute("href"));
+
+            }
+
+
+            // Проверяем сортировку стран
+            List<string> CountriesSorted = new List<string>(Countries);
+            CountriesSorted.Sort();
+
+            if (Countries.SequenceEqual(CountriesSorted)) Console.WriteLine("Countries are sorted correctly");
+            else Console.WriteLine("Countries are not sorted correctly");
+
+
+            // Проверяем сортировку в зонах
+            foreach (string link in Links)
+            {
+                driver.Url = link;
+
+                var Names = driver.FindElements(By.XPath("//table[@id='table-zones']/tbody/tr/td[3]"));
+                List<string> ZoneNames = new List<string>();
+
+                foreach (IWebElement name in Names)
+                {
+                    if (name.GetAttribute("textContent") != "")
+                    {
+                        ZoneNames.Add(name.GetAttribute("textContent"));
+                    }
+                }
+
+                List<string> ZoneNamesSorted = new List<string>(ZoneNames);
+                ZoneNamesSorted.Sort();
+                if (ZoneNames.SequenceEqual(ZoneNamesSorted)) Console.WriteLine("Zones are sorted correctly");
+                else Console.WriteLine("Zones are not sorted correctly");
+
+
+            }
+
+
         }
 
         [TearDown]
