6d83549f64f4901e6cd40e401ed2663b43fdccb6
Ekaterina Tolchanova <sunegina.katerin@mail.ru>
2023-06-23 21:12:25 +0300

    Задание 12. Добавить новую утку

--- QA_lesson_1/UnitTest1.cs
+++ QA_lesson_1/UnitTest1.cs
@@ -1,10 +1,10 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.IE;
 using OpenQA.Selenium.Support.UI;
+using Assert = NUnit.Framework.Assert;
+using System.IO;
+using System.Threading;
 
 namespace QA_lesson_1
 {
@@ -17,15 +17,87 @@ namespace QA_lesson_1
         [SetUp]
         public void start()
         {
-            driver = new InternetExplorerDriver();
-            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-        }
+            driver = new ChromeDriver();
 
+        }
         [Test]
-        public void FirstTest()
+
+        public void AddDuck()
         {
-            driver.Url = "http://www.google.com/";
-            driver.FindElement(By.Name("q")).SendKeys("webdriver" + Keys.Enter);
+
+            // Входим на сайт
+            driver.Url = "http://localhost/litecart/admin/";
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            driver.FindElement(By.Name("login")).Click();
+
+            // Посчитаем, сколько товаров с нашим именем уже есть в каталоге
+            driver.Url = "http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1";
+            var elements = driver.FindElements(By.XPath("//table[@class='dataTable']//tr//td//a"));
+            int elemCount = 0;
+            foreach (IWebElement element in elements)
+            {
+                if (element.GetAttribute("textContent") == "Pirate Duck") elemCount++;
+            }
+
+            // Заходим в "add new product"
+            driver.FindElement(By.XPath("//ul[@id='box-apps-menu']/li[2]/a")).Click();
+            driver.FindElement(By.XPath("//td[@id='content']/div[1]/a[2]")).Click();
+
+
+            // Заполняем General
+            string imgPath = Path.GetFullPath("pirate_duck.jpg");
+
+            driver.FindElement(By.XPath("//div[@id='tab-general']/table/tbody/tr[1]/td/label[1]/input")).Click();
+            driver.FindElement(By.XPath("//div[@id='tab-general']/table/tbody/tr[2]/td/span/input")).SendKeys("Pirate Duck");
+            driver.FindElement(By.XPath("//div[@id='tab-general']/table/tbody/tr[3]/td/input")).SendKeys("qd001");
+            driver.FindElement(By.XPath("//div[@id='tab-general']/table/tbody/tr[4]/td/div/table/tbody/tr[2]/td[1]/input")).Click();
+            driver.FindElement(By.XPath("//div[@id='tab-general']/table/tbody/tr[4]/td/div/table/tbody/tr[1]/td[1]/input")).Click();
+            driver.FindElement(By.XPath("//div[@id='tab-general']/table/tbody/tr[8]//input")).SendKeys("100");
+
+            SelectElement statusSelect = new SelectElement(driver.FindElement(By.XPath("//div[@id='tab-general']/table/tbody/tr[8]//select[@name='sold_out_status_id']")));
+            statusSelect.SelectByValue("2");
+
+            driver.FindElement(By.XPath("//div[@id='tab-general']/table/tbody/tr[10]//input")).SendKeys("17022023");
+            driver.FindElement(By.XPath("//div[@id='tab-general']/table/tbody/tr[11]//input")).SendKeys("17122023");
+            driver.FindElement(By.XPath("//div[@id='tab-general']/table/tbody/tr[9]//input")).SendKeys(imgPath);
+            driver.FindElement(By.XPath("//div[@id='tab-general']/table/tbody/tr[9]//a")).Click();
+
+            // переходим в Information
+            driver.FindElement(By.XPath("//ul[@class='index']/li[2]/a")).Click();
+            Thread.Sleep(500);
+
+            SelectElement manufacturer = new SelectElement(driver.FindElement(By.XPath("//select[@name='manufacturer_id']")));
+            manufacturer.SelectByValue("1");
+            driver.FindElement(By.XPath("//input[@name='keywords']")).SendKeys("queen");
+            driver.FindElement(By.XPath("//input[@name='short_description[en]']")).SendKeys("test");
+            driver.FindElement(By.XPath("//div[@class='trumbowyg-editor']")).SendKeys("test");
+            driver.FindElement(By.XPath("//input[@name='head_title[en]']")).SendKeys("test");
+            driver.FindElement(By.XPath("//input[@name='meta_description[en]']")).SendKeys("test");
+
+            // Переходим в Prices
+            driver.FindElement(By.XPath("//ul[@class='index']/li[4]/a")).Click();
+            Thread.Sleep(500);
+            driver.FindElement(By.XPath("//input[@name='purchase_price']")).SendKeys("40");
+            SelectElement price = new SelectElement(driver.FindElement(By.XPath("//select[@name='purchase_price_currency_code']")));
+            price.SelectByValue("USD");
+            driver.FindElement(By.XPath("//input[@name='prices[USD]']")).SendKeys("40");
+            driver.FindElement(By.XPath("//input[@name='prices[EUR]']")).SendKeys("40");
+            driver.FindElement(By.XPath("//button[@name='save']")).Click();
+            Thread.Sleep(500);
+
+            // Проверим, что новый товар с нашим именем появился
+            driver.Url = "http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1";
+            elements = driver.FindElements(By.XPath("//table[@class='dataTable']//tr//td//a"));
+            int newElemCount = 0;
+            foreach (IWebElement element in elements)
+            {
+                if (element.GetAttribute("textContent") == "Pirate Duck") newElemCount++;
+            }
+            Assert.AreEqual(newElemCount, elemCount + 1);
+
+
+
         }
 
         [TearDown]
@@ -34,5 +106,6 @@ namespace QA_lesson_1
             driver.Quit();
             driver = null;
         }
+
     }
 }
