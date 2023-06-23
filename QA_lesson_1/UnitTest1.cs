3d0a16b714e10bc8eb98e2156bdee0ca38d5590f
Ekaterina Tolchanova <sunegina.katerin@mail.ru>
2023-06-23 23:59:38 +0300

    Задание 17. Проверка логов

--- QA_lesson_1/UnitTest1.cs
+++ QA_lesson_1/UnitTest1.cs
@@ -1,10 +1,10 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.IE;
 using OpenQA.Selenium.Support.UI;
+using Assert = NUnit.Framework.Assert;
+using System;
+
 
 namespace QA_lesson_1
 {
@@ -17,15 +17,63 @@ namespace QA_lesson_1
         [SetUp]
         public void start()
         {
-            driver = new InternetExplorerDriver();
+            driver = new ChromeDriver();
             wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
         }
 
         [Test]
-        public void FirstTest()
+        public void LogCheck()
         {
-            driver.Url = "http://www.google.com/";
-            driver.FindElement(By.Name("q")).SendKeys("webdriver" + Keys.Enter);
+            // Входим на сайт
+            driver.Url = "http://localhost/litecart/admin/";
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            driver.FindElement(By.Name("login")).Click();
+
+            // Пройдем по всем товарам в каталоге
+            driver.Url = "http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1";
+            var elements = driver.FindElements(By.XPath("//table[@class='dataTable']//tr/td[3]/a"));
+
+            for (int i = 0; i < elements.Count; i++)
+            {
+                var element = elements[i];
+
+                // Если зашли в подкатегорию
+                if (element.GetAttribute("textContent") == "Subcategory")
+                {
+                    var URL = element.GetAttribute("href");
+                    var elemCount = elements.Count;
+
+                    element.Click();
+
+                    elements = driver.FindElements(By.XPath("//table[@class='dataTable']//tr/td[3]/a"));
+                    var newElemCount = elements.Count;
+                    var num = i;
+
+                    for (int j = 0; j <= (newElemCount - elemCount); j++)
+                    {
+                        elements[num].Click();
+
+                        Assert.AreEqual(0, driver.Manage().Logs.GetLog("browser").Count);
+
+                        driver.Url = URL;
+                        elements = driver.FindElements(By.XPath("//table[@class='dataTable']//tr/td[3]/a"));
+                        num++;
+
+                    }
+
+                    driver.Url = "http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1";
+                    elements = driver.FindElements(By.XPath("//table[@class='dataTable']//tr/td[3]/a"));
+                }
+                else
+                {
+                    element.Click();
+                    Assert.AreEqual(0, driver.Manage().Logs.GetLog("browser").Count);
+
+                    driver.Url = "http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1";
+                    elements = driver.FindElements(By.XPath("//table[@class='dataTable']//tr/td[3]/a"));
+                }
+            }
         }
 
         [TearDown]
