fad8a9946b53d7be10e5bdf30d63caa5dd097ab8
Ekaterina Tolchanova <sunegina.katerin@mail.ru>
2023-06-23 23:54:04 +0300

    задание 14. Проверка открытия в новом окне

--- QA_lesson_1/UnitTest1.cs
+++ QA_lesson_1/UnitTest1.cs
@@ -1,15 +1,14 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using NUnit.Framework;
-using OpenQA.Selenium;
+﻿using NUnit.Framework;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.IE;
 using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Threading;
 
-namespace QA_lesson_1
+namespace Selenium_Tests
 {
-    [TestFixture]
-    public class MyFirstTest
+    internal class MyFirstTest
     {
         private IWebDriver driver;
         private WebDriverWait wait;
@@ -17,22 +16,65 @@ namespace QA_lesson_1
         [SetUp]
         public void start()
         {
-            driver = new InternetExplorerDriver();
+            driver = new ChromeDriver();
             wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
         }
-
-        [Test]
-        public void FirstTest()
+        static string findNewWindow(IWebDriver driver, ICollection<string> oldWindows, int maxRetryCount = 100)
         {
-            driver.Url = "http://www.google.com/";
-            driver.FindElement(By.Name("q")).SendKeys("webdriver" + Keys.Enter);
-        }
+            bool boolReturnValue;
+            string otherWindow = null;
+            for (var i = 0; i < maxRetryCount; Thread.Sleep(10), i++)
+            {
 
+                ICollection<string> allWindows = driver.WindowHandles;
+                boolReturnValue = (oldWindows.Equals(allWindows) ? true : false);
+
+                if (!boolReturnValue)
+                {
+                    foreach (var window in allWindows)
+                    {
+                        if (!oldWindows.Contains(window)) otherWindow = window;
+                        if (otherWindow != null) return otherWindow;
+                    }
+
+                }
+            }
+
+            throw new ApplicationException("New window did not open.");
+        }
+        [Test]
+
+        public void New_Window()
+        {
+            // Входим на сайт
+            driver.Url = "http://localhost/litecart/admin/";
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            driver.FindElement(By.Name("login")).Click();
+
+            driver.Url = "http://localhost/litecart/admin/?app=countries&doc=countries";
+            driver.FindElement(By.XPath("//td[@id='content']//tr[@class='row']//a")).Click();
+            var elements = driver.FindElements(By.XPath("//i[@class='fa fa-external-link']"));
+
+            string mainWindow = driver.CurrentWindowHandle;
+
+            foreach (var element in elements)
+            {
+                ICollection<string> oldWindows = driver.WindowHandles;
+                element.Click();
+                string newWindow = findNewWindow(driver, oldWindows);
+                driver.SwitchTo().Window(newWindow);
+                Thread.Sleep(1000);
+                driver.Close();
+                driver.SwitchTo().Window(mainWindow);
+            }
+        }
         [TearDown]
         public void stop()
         {
             driver.Quit();
             driver = null;
         }
+
     }
 }
